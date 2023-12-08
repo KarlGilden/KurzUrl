c63b9167abb58b492c5d365c64199298373ed8b2
KarlGilden <karl.gildenhuys@gmail.com>
2023-12-08 20:32:00 +1300

    .

--- client/redirect.js
+++ client/redirect.js
@@ -2,11 +2,13 @@ import * as util from './util.js'
 
 const redirect = async (slug) => {
     const redirectUrl = await util.getOriginalUrl(slug);
+    console.log("REDIRECT: ", redirectUrl)
     window.location.href = redirectUrl;
 };
 
 const init = () => {
     const slug = window.location.pathname.split("/")[1];
+    console.log("SLUG: ",slug)
     redirect(slug);
 };
 

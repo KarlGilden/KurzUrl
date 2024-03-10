02bdbc53f0e540b8edf181365f3dbfb5ddf00720
KarlGilden <karl.gildenhuys@gmail.com>
2024-03-10 13:52:31 +1300

    refactor and copy pop up

--- client/index.js
+++ client/index.js
@@ -4,26 +4,57 @@ import * as util from './util.js'
 
 const copyUrl = () => {
     // Get the text field
-    var copyText = document.getElementById("short-url");
-  
+    const copyText = document.getElementById("short-url");
+
     // Copy the text inside the text field
-    navigator.clipboard.writeText(copyText.textContent);
-  };
+    navigator.clipboard.writeText(copyText.innerText);
 
-const displayResult = (result) => {
-    outputContainer.innerHTML = `
-        <div class="output">
-            <a href="${result}"><p>${result}</p></a>
-            <button class="btn-copy"><i class="fa fa-clone"></i></button>
-        </div>
-    `
+    // display tool tip
+    displayCopied();
 };
 
-const displayMessage = (msg) => {
-    outputContainer.innerHTML = `<p>${msg}</p>`
+const displayCopied = () => {
+    outputContainer.appendChild(tooltip);
+
+    setTimeout(()=>{
+        outputContainer.removeChild(tooltip);
+    }, 3000);
 };
 
+const displayOutput = (resultContainer) => {
+    outputContainer.replaceChildren(resultContainer);
+};
 
+const buildMessage = (result) => {
+    const textNode = document.createElement("p");
+    textNode.innerText = result;
+    return textNode;
+};
+
+const buildResult = (result) => {
+    const resultDiv = document.createElement("div");
+    const link = document.createElement("a");
+    const copyIcon = document.createElement("i");
+    const copyBtn = document.createElement("button");
+
+    resultDiv.className = "output";
+
+    link.href = result;
+    link.id = "short-url"
+    link.innerText = result;
+
+    copyIcon.className = "fa fa-clone"
+
+    copyBtn.addEventListener("click", copyUrl);
+    copyBtn.className = "btn-copy";
+    copyBtn.id = "btn-copy";
+
+    copyBtn.appendChild(copyIcon);
+    resultDiv.appendChild(link);
+    resultDiv.appendChild(copyBtn);
+
+    return resultDiv;
+}
 
 
 // globals
@@ -31,24 +62,32 @@ const baseURL = window.location.href;
 const urlForm = document.getElementById("form-url");
 const outputContainer = document.getElementById("container-output");
 
+const tooltip = document.createElement("div");
+tooltip.id = "tooltip";
+tooltip.innerText = "Copied!";
+tooltip.className = "tooltip";
+
 // submit listener
 urlForm.addEventListener("submit", async (e)=>{
     e.preventDefault();
 
-    displayMessage("Loading...");
+    displayOutput(buildMessage("Loading..."));
 
     let urlInput = document.getElementById("input-url");
+    let output;
 
     const isUrlValid = util.validateUrl(urlInput.value);
 
     if(isUrlValid){
         const shortUrl = await util.getShortUrl(urlInput.value);
         if(shortUrl){
-            displayResult(baseURL + shortUrl.slug);
+            output = buildResult(baseURL + shortUrl.slug);
         }else{
-            displayMessage("Error getting short url");
+            output = buildMessage("Error getting short url");
         }
     }else{
-        displayMessage("Invalid Url");
+        output = buildMessage("Invalid Url");
     }
+
+    displayOutput(output);
 });

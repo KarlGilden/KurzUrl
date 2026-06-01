e5b32fa04f7fcb7afaf931d4111c58a544afab5c
KarlGilden <karl.gildenhuys@gmail.com>
2026-06-01 21:29:45 +1200

    update shape of data

--- client/util.js
+++ client/util.js
@@ -1,37 +1,35 @@
-const baseAPI = "https://kurzurlservice.azurewebsites.net"//"https://localhost:7114";
+const baseAPI = "https://kurzurlservice.azurewebsites.net";
 
 // returns url object
 export const getShortUrl = async (url) => {
-    try{
-        const shortUrl = await fetch(`${baseAPI}/Url`, {
-            method: "POST",
-            headers: {"Content-Type": "application/json"},
-            body: JSON.stringify({
-                originalUrl: url,
-            })
-        });
-        return await shortUrl.json();
-    }catch(error){
-        console.log(error)
-    }
-
-
-}
+	try {
+		const shortUrl = await fetch(`${baseAPI}/Url`, {
+			method: "POST",
+			headers: { "Content-Type": "application/json" },
+			body: JSON.stringify({
+				destinationUrl: url,
+			}),
+		});
+		return await shortUrl.json();
+	} catch (error) {
+		console.log(error);
+	}
+};
 
 // returns url object
 export const getOriginalUrl = async (slug) => {
-    const originalUrl = await fetch(`${baseAPI}/Url/${slug}`, {
-        method: "GET"
-    });
+	const originalUrl = await fetch(`${baseAPI}/Url/${slug}`, {
+		method: "GET",
+	});
 
-    return await originalUrl.json();
+	return await originalUrl.json();
 };
 
 export const validateUrl = (url) => {
-    try{
-        new URL("/", url);
-        return true;
-    }catch{
-        return false;
-    }
-}
+	try {
+		new URL("/", url);
+		return true;
+	} catch {
+		return false;
+	}
+};
